6008e384b59e00ed1b600823ed49b0336ebdab92
kenny516 <126701465+kenny516@users.noreply.github.com>
2024-05-07 14:14:40 +0600

    commit sprint 0 2409
    creation de la class Front Controller avec le script .bat pour la creation du controller

---
+++ Build_jar/create_jar.bat
@@ -0,0 +1,33 @@
+@echo off
+
+rem Chemin vers le dossier contenant les fichiers .class
+set CLASS_DIR=D:\FrameWork Java Web\out
+
+rem Nom du fichier JAR de destination (modifiable selon vos besoins)
+set JAR_FILE=Framework.jar
+
+rem Vérification si le dossier contenant les classes existe
+if not exist "%CLASS_DIR%" (
+    echo Le dossier spécifié n'existe pas.
+    exit /b 1
+)
+
+rem Vérification si le dossier contenant les classes est vide
+dir /b "%CLASS_DIR%" | findstr /r "\.class$" > nul
+if errorlevel 1 (
+    echo Le dossier spécifié ne contient pas de fichiers .class.
+    exit /b 1
+)
+
+rem Vérification si le fichier JAR de destination existe déjà
+if exist "%JAR_FILE%" (
+    echo Le fichier JAR de destination existe déjà. Veuillez supprimer le fichier existant avant de continuer.
+    exit /b 1
+)
+
+rem Création du fichier JAR
+echo Création du fichier JAR...
+jar cf "%JAR_FILE%" -C "%CLASS_DIR%" .
+
+echo Le fichier JAR a été créé avec succès.
+pause
