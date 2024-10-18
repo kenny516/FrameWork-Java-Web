2f78ad4901366615ddaf8a4e8655657540f96700
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2024-10-18 22:28:43 +0300

    upload

--- src/Utils/Requestparam.java
+++ src/Utils/Requestparam.java
@@ -1,8 +1,12 @@
 package Utils;
 
 import Annotation.Param;
+import Utils.UploadFile.UploadFileTools;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
@@ -10,76 +14,108 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 
 public class Requestparam {
-    HttpServletRequest request;
+    private final HttpServletRequest request;
 
     public Requestparam(HttpServletRequest request) {
         this.request = request;
     }
 
-    public Object mappingParam(Parameter param,String name) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Enumeration<String> parametersNames = request.getParameterNames();
-        ArrayList<String> listOfparamObj = new ArrayList<>();
-        // verification si c est un objet
-        // Parcourir tous les paramètres de la requête
-        while (parametersNames.hasMoreElements()) {
-            String nameParam = parametersNames.nextElement();
-            String[] valueName = nameParam.split("\\.");
+    public Object mappingParam(Parameter param, String name) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, ServletException, IOException {
+        ArrayList<String> listOfParamObj = collectObjectParameters(param, name);
 
-            // Vérifier si le paramètre appartient à un objet
-            if (valueName.length > 1) {
-                if (valueName[0].equals(name)) {
-                    listOfparamObj.add(nameParam);
-                } else if (param.isAnnotationPresent(Param.class)) {
-                    if (valueName[0].equals(param.getAnnotation(Param.class).name())) {
-                        listOfparamObj.add(nameParam);
-                    }
-                }
+        if (param.getType() == Part.class) {
+//            throw new ServletException("ici "+param.getAnnotation(Param.class).name() + " "+request.getPart(param.getAnnotation(Param.class).name()));
+            if (UploadFileTools.isUpload(request, param.getAnnotation(Param.class).name())) {
+                return request.getPart(param.getAnnotation(Param.class).name());
+            }else {
+                return null;
             }
         }
-        //si c est un objet
-        if (listOfparamObj.size() > 0) {
+
+
+        // Check if it's an object
+        if (!listOfParamObj.isEmpty()) {
             Object paramInstance = param.getType().getDeclaredConstructor().newInstance();
             Method[] methods = param.getType().getDeclaredMethods();
 
-            for (String paramObj : listOfparamObj) {
-                String paramObjGET = request.getParameter(paramObj);
-                for (Method method : methods) {
-                    if (method.getName().equals("set" +  Tools.capitalize(paramObj.split("\\.")[1])))  {
-                        if (method.getParameters()[0].getType() == int.class) {
-                            method.invoke(paramInstance, Integer.valueOf(paramObjGET));
-                        } else {
-                            method.invoke(paramInstance,paramObjGET);
-                        }
-                    }
+            for (String paramObj : listOfParamObj) {
+                String paramValue = request.getParameter(paramObj);
+                String fieldName = paramObj.split("\\.")[1];
+                Method setterMethod = findSetterMethod(methods, fieldName);
+
+                if (setterMethod != null) {
+                    Class<?> paramType = setterMethod.getParameters()[0].getType();
+                    Object castedValue = castValue(paramValue, paramType);
+                    setterMethod.invoke(paramInstance, castedValue);
                 }
             }
             return paramInstance;
         }
-        //sinon
-        else {
-            if (request.getParameter(name) != null) {
-                return request.getParameter(name);
-            }
-            //avec annotation
-            else if (param.isAnnotationPresent(Param.class)) {
-                if (request.getParameter(param.getAnnotation(Param.class).name()) != null) {
-                    return request.getParameter(param.getAnnotation(Param.class).name());
-                } else {
-                    return null;
-                }
-            }
-            return null;
-        }
+
+        // Otherwise, handle it as a basic type or annotated parameter
+        return getParameterValue(param, name);
     }
 
-    public Object castParam(String parameter, Class<?> clazz) {
-        Object retour = new Object();
-        if (clazz == int.class) {
-            retour = Integer.valueOf(parameter);
+    private ArrayList<String> collectObjectParameters(Parameter param, String name) {
+        ArrayList<String> listOfParamObj = new ArrayList<>();
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        // Collect parameters that belong to the object
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            String[] valueNameParts = paramName.split("\\.");
+
+            if (valueNameParts.length > 1 && valueNameParts[0].equals(name)) {
+                listOfParamObj.add(paramName);
+            } else if (param.isAnnotationPresent(Param.class) && valueNameParts[0].equals(param.getAnnotation(Param.class).name())) {
+                listOfParamObj.add(paramName);
+            }
+        }
+        return listOfParamObj;
+    }
+
+    private Method findSetterMethod(Method[] methods, String fieldName) {
+        String expectedMethodName = "set" + Tools.capitalize(fieldName);
+        for (Method method : methods) {
+            if (method.getName().equals(expectedMethodName)) {
+                return method;
+            }
+        }
+        return null; // If no setter found
+    }
+
+    private Object castValue(String value, Class<?> paramType) {
+        if (paramType == int.class || paramType == Integer.class) {
+            return Integer.valueOf(value);
+        } else if (paramType == double.class || paramType == Double.class) {
+            return Double.valueOf(value);
+        } else if (paramType == long.class || paramType == Long.class) {
+            return Long.valueOf(value);
+        } else if (paramType == boolean.class || paramType == Boolean.class) {
+            return Boolean.valueOf(value);
+        } else if (paramType == float.class || paramType == Float.class) {
+            return Float.valueOf(value);
+        } else if (paramType == short.class || paramType == Short.class) {
+            return Short.valueOf(value);
+        } else if (paramType == byte.class || paramType == Byte.class) {
+            return Byte.valueOf(value);
         } else {
-            retour = parameter;
+            // For String and other types
+            return value;
         }
-        return retour;
     }
 
+    private Object getParameterValue(Parameter param, String name) {
+        // Check if the parameter exists in the request
+        if (request.getParameter(name) != null) {
+            return request.getParameter(name);
+        }
+        // Handle annotated parameter
+        if (param.isAnnotationPresent(Param.class)) {
+            String paramName = param.getAnnotation(Param.class).name();
+            return request.getParameter(paramName);
+        }
+
+        return null;
+    }
 }
